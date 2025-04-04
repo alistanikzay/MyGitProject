740541f283981a32f1be94af56fb9e81119ff7f7
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-04-04 16:51:15 +0200

    Update pre-commit.ps1

--- .githooks/pre-commit.ps1
+++ .githooks/pre-commit.ps1
@@ -1,10 +1,8 @@
 # Pre-commit hook för filstorlekskontroll
 $maxSize = 1MB
 $exitCode = 0
-
 # Hämta ändrade filer
 $files = git diff --cached --name-only
-
 foreach ($file in $files) {
     if (Test-Path $file -PathType Leaf) {
         $size = (Get-Item $file).Length
@@ -14,9 +12,7 @@ foreach ($file in $files) {
         }
     }
 }
-
 if ($exitCode -eq 0) {
     Write-Host "Alla filer är mindre än 1MB" -ForegroundColor Green
 }
-
 exit $exitCode
